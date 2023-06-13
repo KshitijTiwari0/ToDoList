ec2594b00511e634725be4d28468f1ce17415fe7
KshitijTiwari0 <satyarupkshitij3002@gmail.com>
2023-06-13 18:06:30 +0530

    first commit

--- app.js
+++ app.js
@@ -1,36 +1,46 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const date = require(__dirname +"/date.js");
+let items = [];
+let workItems = [];
 const app = express();
 app.set("view engine", "ejs");
-
+app.use(bodyParser.urlencoded({extended:true}));
+//To access the static folder
+app.use(express.static("public"));
 app.get("/", function(req, res) {
-  var today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
+ 
+  let day = date.getDate();
+  
+  res.render("list", { listTitle: day,newListItems:  items });
+});
+app.post("/",function(req,res){
+  let item =  req.body.newItem; 
+  
+if (req.body.list === "Work"){
+  workItems.push(item);
+  res.redirect("/work");
+}else {
+  items.push(item);
+  res.redirect("/");
+}
 
-  switch(currentDay){
-    case 0:
-       day = "Sunday";
-    case 1:
-       day = "Monday";
-    case 2:
-       day = "Tuesday";
-    case 3:
-       day = "Wednesday";
-    case 4:
-       day = "Thursday";
-    case 5:
-       day = "Friday";
-    case 6:
-       day = "Saturday";
-    break;
-    default:
-    console.log("Error current is equal to "+currentDay);
-  }
-  res.render("list", { kindOfDay: day });
+  
 });
 
+app.get("/work",function(req,res){
+  res.render("list",{listTitle:"Work List",newListItems:workItems})
+});
+
+app.get("/about",function(req,res){
+   res.render("about");
+});
+
+app.post("/work",function(req,res){
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+});
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
